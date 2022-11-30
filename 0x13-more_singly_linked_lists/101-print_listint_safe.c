653eaec6661f075a0e3b9b85ecc101472c87f719
Jessicasaduwa <jessicasaduwa@gmail.com>
2022-11-30 13:03:54 -0800

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -51,3 +51,45 @@ size_t looped_listint_len(const listint_t *head)
 
 	return (0);
 }
+
+/**
+ * free_listint_safe - Frees a listint_t list safely
+ *		(i.e can free lists containing loops).
+ * @h: pointer to the address of the head of the listint_t list.
+ *
+ * Return: the size of the list that was freed.
+ * Description: The function sets the head to NULL.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t nodes, index;
+
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
+	{
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
+}
