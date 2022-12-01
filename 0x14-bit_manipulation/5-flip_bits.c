83f6247f27351560346c5dc101481ca756ea4407
Jessicasaduwa <jessicasaduwa@gmail.com>
2022-12-01 15:55:33 -0800

    a function that returns the number of bits you would need to flip to get from one number to another.

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,7 @@
 
 /**
  * flip_bits -  returns the number of bits you would
- * 		need to flip to get from one number to another.
+ *		need to flip to get from one number to another.
  * @n: number one.
  * @m: number two.
  *
